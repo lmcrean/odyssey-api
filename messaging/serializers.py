1d44d0a84da51ccd95214d8da072807c83a5d569
Laurie Crean <lmcrean@gmail.com>
2024-08-08 16:33:31 +0000

    Add formatted_timestamp to MessageSerializer, Removed unused recipient_id field.
    - Added formatted_timestamp field to MessageSerializer to format the timestamp as '%d %b %Y %H:%M'.
    - Updated MessageSerializer fields and read_only_fields to include formatted_timestamp.
    - Removed unused recipient_id field.

--- messaging/serializers.py
+++ messaging/serializers.py
@@ -4,14 +4,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from messaging.models import Message
 
-class MessageSerializer(serializers.ModelSerializer):
-    recipient_id = serializers.IntegerField(write_only=True)
 
 class MessageSerializer(serializers.ModelSerializer):
+    formatted_timestamp = serializers.SerializerMethodField()
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'recipient', 'content', 'image', 'timestamp', 'read']
-        read_only_fields = ['id', 'sender', 'timestamp', 'read', 'recipient']
+        fields = ['id', 'sender', 'recipient', 'content', 'image', 'formatted_timestamp', 'read']
+        read_only_fields = ['id', 'sender', 'formatted_timestamp', 'read', 'recipient']
     
     def get_formatted_timestamp(self, obj):
         return obj.timestamp.strftime('%d %b %Y %H:%M')
