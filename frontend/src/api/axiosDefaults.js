862a042942f4c4c946d9b962adf320ac9c0518ec
Laurie Crean <lmcrean@gmail.com>
2024-09-12 15:29:55 +0000

    Refactor axiosDefaults.js to update baseURL to /api

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://odyssey-api-f3455553b29d.herokuapp.com/";
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
