3f650a4c792e3aa7fe854b9b37cf7f41afb5c99e
Laurie Crean <lmcrean@gmail.com>
2024-09-13 18:06:03 +0000

    Chore: add testing video for cypress user_journey

--- frontend/cypress/support/component.js
+++ frontend/cypress/support/component.js
@@ -1,3 +1,6 @@
+// frontend/cypress/support/component.js
+// This file is used to add custom commands and utilities to the Cypress test runner.
+
 import { BrowserRouter } from 'react-router-dom'
 
 Cypress.Commands.add('mount', (component, options = {}) => {
