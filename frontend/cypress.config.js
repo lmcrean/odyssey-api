79a3e71f1d71cf1dd66d7621a9c3108516040bda
Laurie Crean <lmcrean@gmail.com>
2024-09-13 17:55:16 +0000

    Refactor frontend Cypress configuration file

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,10 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      
     },
     baseUrl: 'http://localhost:8000',
-    supportFile: false,
+    supportFile: 'cypress/support/component.js',
     screenshotOnRunFailure: true,
+    video: true,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
   },
 });
