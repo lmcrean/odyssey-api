2d0d27e8d39414aa1964f0281ae08203332e3b02
Laurie Crean <lmcrean@gmail.com>
2024-09-13 14:51:19 +0000

    update playwright.config.js with correct base URL

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,39 +1,20 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './playwright',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
+  forbidOnly: !!process.env.CI, 
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: process.env.CI ? 1 : undefined, 
   reporter: [['list'], ['json', { outputFile: 'test-results.json' }]],
   outputDir: 'screenshots',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://3000-lmcrean-momentsclone2-chab47zjuwx.ws.codeinstitute-ide.net/',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    command: 'python3 manage.py runserver',
+    baseURL: 'http://localhost:8000',
     trace: 'on-first-retry',
-    screenshot: 'on', // or 'on', depending on your needs
+    screenshot: 'on',
   },
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -49,32 +30,10 @@ module.exports = defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'https://3000-lmcrean-momentsclone2-chab47zjuwx.ws.codeinstitute-ide.net/',
+    command: 'python3 manage.py runserver',
+    url: 'http://localhost:8000',
     reuseExistingServer: !process.env.CI,
   },
 });
