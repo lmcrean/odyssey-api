93995a55e321d3cc648e11bc6142d76a7b1e8cd2
Laurie Crean <lmcrean@gmail.com>
2024-09-16 16:57:54 +0000

    Refactor MessageUpdateTests for API compatibility
    - Use reverse() for dynamic URL generation

--- messaging/tests/tests_message_update.py
+++ messaging/tests/tests_message_update.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from messaging.models import Message
+from django.urls import reverse
 
 class MessageUpdateTests(APITestCase):
 
@@ -18,7 +19,7 @@ class MessageUpdateTests(APITestCase):
         self.client.login(username='user1', password='password')
 
     def test_update_message_success(self):
-        url = f'/messages/{self.message.id}/update/'
+        url = reverse('message-update', kwargs={'pk': self.message.id})
         data = {'content': 'Updated message content'}
         response = self.client.patch(url, data, format='json')
         self.message.refresh_from_db()
@@ -28,7 +29,7 @@ class MessageUpdateTests(APITestCase):
     def test_update_message_unauthorized(self):
         self.client.logout()
         self.client.login(username='user2', password='password')
-        url = f'/messages/{self.message.id}/update/'
+        url = reverse('message-update', kwargs={'pk': self.message.id})
         data = {'content': 'Updated message content'}
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
