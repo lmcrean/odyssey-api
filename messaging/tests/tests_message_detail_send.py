9ff172e5a23590086a4c4c6e20adc91bd1057568
Laurie Crean <lmcrean@gmail.com>
2024-09-16 16:59:01 +0000

    Update MessageSendTests in tests_message_detail_send.py
    - Implement reverse() for proper URL resolution
    - Adjust test_send_message to use correct API endpoint
    - Modify test_receive_messages to parse JSON response
    - Ensure compatibility with the expected API response structure for message sending and retrieval

--- messaging/tests/tests_message_detail_send.py
+++ messaging/tests/tests_message_detail_send.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 from messaging.models import Message
+from django.urls import reverse
 
 class MessageSendTests(APITestCase):
     def setUp(self):
@@ -12,11 +13,9 @@ class MessageSendTests(APITestCase):
         self.client.login(username='user1', password='password')
 
     def test_send_message(self):
-        url = f'/messages/{self.user2.id}/send/'
+        url = reverse('message-detail-send', kwargs={'user_id': self.user2.id})
         data = {'content': 'Hello!', 'recipient_id': self.user2.id}
         response = self.client.post(url, data, format='json')
-
-
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Message.objects.count(), 1)
         self.assertEqual(Message.objects.get().content, 'Hello!')
@@ -25,11 +24,9 @@ class MessageSendTests(APITestCase):
         Message.objects.all().delete()
         Message.objects.create(sender=self.user1, recipient=self.user2, content='Hello!')
         self.client.login(username='user2', password='password')
-        url = f'/messages/{self.user1.id}/'
-        response = self.client.get(url, format='json')
-
-
-        # Adjusting the assertion to match the actual response structure
+        url = reverse('message-detail', kwargs={'user_id': self.user1.id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['results']), 1)
-        self.assertEqual(response.data['results'][0]['content'], 'Hello!')
+        content = response.json()
+        self.assertEqual(len(content['results']), 1)
+        self.assertEqual(content['results'][0]['content'], 'Hello!')
