5064c6e791c66f5eb54fcbde0aeb1dac5048bb09
Laurie Crean <lmcrean@gmail.com>
2024-10-09 22:21:42 +0000

    DEBUG = False in settings.py, set to False for production

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,7 +65,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+# DEBUG = 'DEBUG' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
