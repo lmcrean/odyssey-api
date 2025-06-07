40615e87aa7e2f1127f5215442bd5c1f56943580
Heroku Deploy <deploy@example.com>
2025-06-07 15:46:52 +0100

    Add WSGI application aliases for Vercel compatibility

--- drf_api/wsgi.py
+++ drf_api/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api.settings')
 
 application = get_wsgi_application()
+
+# Vercel expects either 'app' or 'handler'
+app = application
+handler = application
